d879497d5aee4aaffe571820fc74bbc749312cba
DreamSky1996 <webhipe@gmail.com>
2022-06-17 06:39:29 +0900

    change script code

--- progressbar.py
+++ progressbar.py
@@ -22,15 +22,16 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
 	if iteration == total:
 		print()
 
-# A List of Items
-items = list(range(0, 57))
-l = len(items)
+if __name__ == '__main__':
+	# A List of Items
+	items = list(range(0, 57))
+	l = len(items)
 
-# Initial call to print 0% progress
-printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+	# Initial call to print 0% progress
+	printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-for i, item in enumerate(items):
-	# Do stuff...
-	time.sleep(0.1)
-	# Update Progress Bar
-	printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+	for i, item in enumerate(items):
+		# Do stuff...
+		time.sleep(0.1)
+		# Update Progress Bar
+		printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
